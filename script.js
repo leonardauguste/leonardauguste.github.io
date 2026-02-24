7b9813ca478955b62cbf7dd120cb46709c0b81cb
leonardauguste <leonardaugustee@gmail.com>
2026-02-24 18:44:01 +0700

    FINAL DAY 2

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ document.addEventListener("mousemove",e=>{
 cursor.style.left=e.clientX+"px"
 cursor.style.top=e.clientY+"px"
 })
+
+// FILTER
+function filter(cat){
+document.querySelectorAll(".event").forEach(e=>{
+e.style.display =
+(cat=="all"||e.classList.contains(cat))
+?"flex":"none"
+})
+}
+
+// MODAL IMAGE
+function openModal(el){
+document.getElementById("modal").style.display="flex"
+document.getElementById("modalImg").src=
+el.querySelector("img").src
+}
+
+function closeModal(){
+document.getElementById("modal").style.display="none"
+}
